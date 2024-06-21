a9cb47c85deab48ee22d0bc535ad74efbbef5d2f
Niccar95 <niccar.95@outlook.com>
2024-06-21 13:20:37 +0200

    finished styling for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,26 +3,31 @@ import animalImage from "../assets/giraffe-4456974_1920.jpg";
 export const Home = () => {
   return (
     <>
-      <h1>Välkommen till The Zoo!</h1>
-      <section id="introWrapper">
+      <h1 className="welcomeHeading">Välkommen till The Zoo!</h1>
+      <div id="introWrapper">
         <div className="animalImageContainer">
           <img className="animalImage" src={animalImage} alt="giraffe"></img>
         </div>
-        <p className="zooDescription">
-          Välkommen till "The Zoo" – din främsta destination för att utforska en
-          mångfald av fascinerande varelser från hela världen. Här på "The Zoo"
-          strävar vi efter att förena människor med djur i en atmosfär av
-          lärande, respekt och omsorg. Oavsett om du är nyfiken på de exotiska
-          arterna från fjärran länder eller är på jakt efter en ny familjemedlem
-          bland våra adopterbara vänner, så är du välkommen att utforska våra
-          artfullt designade utställningar och lära känna våra bevingade,
-          skaliga och pälsklädda invånare. Låt "The Zoo" vara din port till en
-          värld av upptäckt och engagemang för djurens välbefinnande.
-        </p>
-      </section>
-      <p>
-        <b>Vi ser fram emot ditt besök!</b>
-      </p>
+
+        <section className="zooDescriptionSection">
+          <p>
+            Välkommen till "The Zoo" – din främsta destination för att utforska
+            en mångfald av fascinerande varelser från hela världen. Här på "The
+            Zoo" strävar vi efter att förena människor med djur i en atmosfär av
+            lärande, respekt och omsorg. Oavsett om du är nyfiken på de exotiska
+            arterna från fjärran länder eller är på jakt efter en ny
+            familjemedlem bland våra adopterbara vänner, så är du välkommen att
+            utforska våra artfullt designade utställningar och lära känna våra
+            bevingade, skaliga och pälsklädda invånare. Låt "The Zoo" vara din
+            port till en värld av upptäckt och engagemang för djurens
+            välbefinnande.
+          </p>
+
+          <p>
+            <b>Vi ser fram emot ditt besök!</b>
+          </p>
+        </section>
+      </div>
     </>
   );
 };
