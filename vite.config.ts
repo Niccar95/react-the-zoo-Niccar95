4090af52bccd58f62cd206badf6ba5203444a592
Niccar95 <niccar.95@outlook.com>
2025-02-20 19:24:45 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-the-zoo-Niccar95",
+  base: "/react-the-zoo-Niccar95/",
   plugins: [react()],
 });
